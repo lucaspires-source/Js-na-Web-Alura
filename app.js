00b5fc133312040261ece5008574e124ecc4539e
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-04 10:21:30 -0300

    Fim da Aula 02.04

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const criarTarefa = (e) => {
     e.preventDefault()  
     const input = document.querySelector('[data-form-input]');
     const valor = input.value;
-    console.log(valor);
+    const tarefa = document.querySelector('[data-task]')
+    const conteudo = `<p class="content">${valor}</p>`
+    tarefa.innerHTML =conteudo
     input.value = ''
 }
 
