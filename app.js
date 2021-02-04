684e9cdc29ae028d0392f639d399273c003a5961
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-04 11:04:15 -0300

    Fim da Aula 04

--- app.js
+++ app.js
@@ -2,13 +2,30 @@
 
 const criarTarefa = (e) => {
     e.preventDefault()  
+    const lista = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]');
     const valor = input.value;
-    const tarefa = document.querySelector('[data-task]')
-    const conteudo = `<p class="content">${valor}</p>`
-    tarefa.innerHTML =conteudo
-    input.value = ''
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task')
+    const conteudo = `<p class="content">${valor}</p>`;
+    tarefa.innerHTML = conteudo;
+    tarefa.appendChild(botaoConclui())
+    lista.appendChild(tarefa);
+    input.value = '';
 }
+
 
 const novaTarefa = document.querySelector('[data-form-button]');
 novaTarefa.addEventListener('click', criarTarefa);
+const concluirTarefa = (e) =>{
+    const botaoConclui = e.target
+    const tarefaCompleta = botaoConclui.parentElement
+    tarefaCompleta.classList.toggle('done')
+}
+const botaoConclui = () =>{
+    const botaoConclui = document.createElement('button');
+    botaoConclui.innerHTML = `Concluir`
+    botaoConclui.classList.add('form-button')
+    botaoConclui.addEventListener('click', concluirTarefa)
+    return botaoConclui
+}
