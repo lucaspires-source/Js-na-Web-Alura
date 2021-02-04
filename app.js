e1e9e06ed0ebfbf96e5d070fcd32280af3be19ea
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-04 09:15:11 -0300

    Fim da Aula 02.04

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const novaTarefa = document.querySelector("[data-form-button]");
 
-novaTarefa.addEventListener("click", (e) => {
-  e.preventDefault()  
-  const input = document.querySelector("[data-form-input]");
-  const valor = input.value;
-  console.log(valor);
-});
+
+const criarTarefa = (e) => {
+    e.preventDefault()  
+    const input = document.querySelector('[data-form-input]');
+    const valor = input.value;
+    console.log(valor);
+    input.value = ''
+}
+
+const novaTarefa = document.querySelector('[data-form-button]');
+novaTarefa.addEventListener('click', criarTarefa);
